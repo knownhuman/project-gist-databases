6492052c373ed05b074994e39d7bdd24b2d53a7e
knownhuman <pbradleyrobb@gmail.com>
2019-03-01 20:31:08 -0500

    All Tests Passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,35 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    r = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    r.raise_for_status()
+
+    query="""
+    INSERT INTO gists(
+        github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url,
+        public, created_at, updated_at, comments, comments_url)
+        VALUES
+        (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url,
+        :forks_url, :public, :created_at, :updated_at, :comments, :comments_url)
+    """
+
+    for i in r.json():
+        params = {
+        'github_id': i['id'],
+        'html_url': i['html_url'],
+        'git_pull_url': i['git_pull_url'],
+        'git_push_url': i['git_push_url'],
+        'commits_url': i['commits_url'],
+        'forks_url': i['forks_url'],
+        'public': i['public'],
+        'created_at': i['created_at'],
+        'updated_at': i['updated_at'],
+        'comments': i['comments'],
+        'comments_url': i['comments_url']
+        }
+
+        db.execute(query, params)
+
+    if commit:
+        db.commit()
+    return
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,16 @@
 from .models import Gist
+import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    if not kwargs:
+        query = 'SELECT * FROM gists'
+    elif 'github_id' in kwargs:
+        query = 'SELECT * FROM gists WHERE github_id = :github_id'
+    elif 'created_at' in kwargs:
+        query = 'SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)'
+
+    cursor = db_connection.execute(query, kwargs)
+    results = []
+    for i in cursor:
+        results.append(Gist(i))
+    return results
